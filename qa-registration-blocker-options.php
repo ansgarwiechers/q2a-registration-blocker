f03a442c4fad80fe5bdcd228dbd4ce14b9682128
Momin Ahmad <42776853+MominRaza@users.noreply.github.com>
2020-11-17 23:11:40 +0530

    Update qa-registration-blocker-options.php

--- qa-registration-blocker-options.php
+++ qa-registration-blocker-options.php
@@ -23,7 +23,7 @@ class qas_ubl_opt {
   const PLUGIN_ACTIVE               = 'qas_ubl_active';
   const SAVE_OPTIONS                = 'qas_ubl_save_options';
   const BANNED_USERNAMES            = 'qas_ubl_banned_usernames';
-  const BANNED_EMAIL_DOMAINS        = 'qas_ubl_banned_email_domains';
+  const ALLOWED_EMAIL_DOMAINS        = 'qas_ubl_allowed_email_domains';
   const BANNED_EMAIL_ADDRESSES      = 'qas_ubl_banned_email_addresses';
   const DONT_ALLOW_TO_CHANGE_EMAIL  = 'qas_ubl_dont_allow_ch_email';
   const DONT_ALLOW_TO_CHANGE_HANDLE = 'qas_ubl_dont_allow_ch_handle';
